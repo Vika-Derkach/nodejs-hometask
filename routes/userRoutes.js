e6e90d17a4bee62da2ef05e7975f3d278f66d75d
Vika-Derkach <queenviktoriyphp@gmail.com>
2021-05-14 21:15:46 +0300

    first commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,23 @@
-const { Router } = require('express');
-const UserService = require('../services/userService');
-const { createUserValid, updateUserValid } = require('../middlewares/user.validation.middleware');
-const { responseMiddleware } = require('../middlewares/response.middleware');
+const { Router } = require("express");
+const UserService = require("../services/userService");
+
+const {
+  createUserValid,
+  updateUserValid,
+} = require("../middlewares/user.validation.middleware");
+const { responseMiddleware } = require("../middlewares/response.middleware");
 
 const router = Router();
 
 // TODO: Implement route controllers for user
+
+router.post("/", function (req, res, next) {
+  const result = UserService.save(req.body);
+  if (result) {
+    res.json(result);
+  } else {
+    res.status(400).send(`some error`);
+  }
+});
 
 module.exports = router;
--- services/userService.js
+++ services/userService.js
@@ -1,16 +1,22 @@
-const { UserRepository } = require('../repositories/userRepository');
+const { UserRepository } = require("../repositories/userRepository");
 
 class UserService {
-
-    // TODO: Implement methods to work with user
-
-    search(search) {
-        const item = UserRepository.getOne(search);
-        if(!item) {
-            return null;
-        }
-        return item;
+  // TODO: Implement methods to work with user
+  save = (user) => {
+    if (user) {
+      return UserRepository.create(user);
+    } else {
+      return null;
     }
+  };
+
+  search(search) {
+    const item = UserRepository.getOne(search);
+    if (!item) {
+      return null;
+    }
+    return item;
+  }
 }
 
 module.exports = new UserService();
